5b5968dc31e689e2b6c6a344082d6da6d0eafb97
Josephine-uwizeye <j.uwizeye@alustudent.com>
2021-02-02 23:00:06 +0200

    question three week one commit

---
+++ DSAQ3amax.py
@@ -0,0 +1,10 @@
+
+
+# body program
+def finding_maximum_value():
+    list1 = [456, 700, 200, 89900, 920202, 27728, 9, 90, 87, 50, 40, 800000]
+    print("Maximum value in a list  : ", max(list1))
+
+
+finding_maximum_value()
+
---
+++ DSAQ3part2.py
@@ -0,0 +1,14 @@
+# Authors: Samwel, Josephine, Modester
+# GitHub handles: @Sammyiel, @Josephine-uwizeye, @Modester-mw
+
+# Question: Make each letter in a string lowercase
+
+
+# function to ask for input and print to lowercase
+def name():
+    input_string = input("What is your name? ")
+    print(input_string.lower())
+
+
+name()
+
